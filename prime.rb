7a962a7920d5fa7a3e3066976ebaeb1618f8f17d
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-29 05:02:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 require "pry"
 def prime?(num)
   num_sqr = num**2
-  (2..num_sqr)
-
+  (2..num_sqr)to_a.each do |i|
+    if num % i == 0 && num < i
+      return false
+  end
+  return true
 end
-binding.pry
