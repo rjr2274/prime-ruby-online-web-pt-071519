dd24e22f166a24d3815f5e6fb392f1979e2a444f
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-29 05:46:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  return false if num <= 1
+  set = (2..num).to_a
+  set.each do |i|
+    if num != i && num % i == 0
+      return false
+    end
+  end
+  return true
+end
